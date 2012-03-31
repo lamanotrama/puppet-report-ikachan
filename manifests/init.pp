28b73c3c9b38345f3b108e0836ab60249933d31b
Gosuke Miyashita <gosukenator@gmail.com>
2012-03-31 13:21:16 +0900

    Create module skelton

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: ikachan
+#
+# This module manages ikachan
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class ikachan {
+
+
+}
